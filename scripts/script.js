0197dfd18ebd0d9ce630773bb0add6842327d1e1
João Tupinambá <95971013+tupis@users.noreply.github.com>
2021-12-28 22:40:16 -0300

    ajustando comunicação de html e js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,44 @@
-var teclas = document.querySelector(".teclas").addEventListener('click', adicionar())
-var arr = []
+var elem_1 = []
+var elem_2 = []
 
-function adicionar(){
-    let teclas = document.querySelector(".teclas").value
-    arr.push(teclas)
-    console.log(arr)
+function adicionar(value){
+    const visor = document.querySelector(".visor")
+    switch (value) {
+        case 0:
+            elem_1.push(0)
+            break;
+        case 1:
+            elem_1.push(1)
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        case 7:
+
+            break;
+        case 8:
+
+            break;
+        case 9:
+
+            break;      
+    }
+    visor.innerHTML = `${elem_1}`
+}
+
+console.log(elem_1)
+
+function limpar(){
+    const visor = document.querySelector(".visor")
+    visor.innerHTML = ""
+    elem_1 = []
 }
