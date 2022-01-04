5d2da29a754da6d4948807a6d18731455b513206
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-01-04 18:48:06 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,10 @@ var elem_1 = []
 var elem_2 = []
 var elemento = 1
 var resultado;
+var termo = elem_2
 
-elem_1.push(9)
-elem_2.push(2)
+/* elem_1.push(9)
+elem_2.push(2) */
 
 const termo1 = String(elem_1).split(",").join(" ").replace(/ /g ,"")
 const termo2 = String(elem_2).split(",").join(" ").replace(/ /g ,"")
@@ -16,37 +17,37 @@ function adicionar(value){
     const visor = document.querySelector(".visor")
     switch (value) {
         case 0:
-            elem_1.push(0)
+            termo.push(0)
             break;
         case 1:
-            elem_1.push(1)
+            termo.push(1)
             break;
         case 2:
-            elem_1.push(2)
+            termo.push(2)
             break;
         case 3:
-            elem_1.push(3)
+            termo.push(3)
             break;
         case 4:
-            elem_1.push(4)
+            termo.push(4)
             break;
         case 5:
-            elem_1.push(5)
+            termo.push(5)
             break;
         case 6:
-            elem_1.push(6)
+            termo.push(6)
             break;
         case 7:
-            elem_1.push(7)
+            termo.push(7)
             break;
         case 8:
-            elem_1.push(8)
+            termo.push(8)
             break;
         case 9:
-            elem_1.push(9)
+            termo.push(9)
             break;      
     }
-    const elemento1 = String(elem_1).split(",").join(" ").replace(/ /g ,"")
+    const elemento1 = String(termo).split(",").join(" ").replace(/ /g ,"")
     visor.innerHTML = `${elemento1}`
 }
 
@@ -78,18 +79,18 @@ function pow(a = Number(termo1) , b = 2){
 function clean(){
     const visor = document.querySelector(".visor")
     visor.innerHTML = ""
-    elem_1 = []
-    elem_2 = []
+    elem_1.length = 0
+    elem_2.length = 0
 }
 
 function back(){
     const visor = document.querySelector(".visor")
-    elem_1.pop()
-    const elemento1 = String(elem_1).split(",").join(" ").replace(/ /g ,"")
+    termo.pop()
+    const elemento1 = String(termo).split(",").join(" ").replace(/ /g ,"")
     visor.innerHTML = `${elemento1}`
     
 }
 
 function result(){
-
+    const visor = document.querySelector(".visor")
 }
