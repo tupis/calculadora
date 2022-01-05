3dd2d1850b322e65d561cbc1deb30afd053f876e
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-01-05 14:40:55 -0300

    restructuring of code

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 // fazer interação do teclado com calculadora
-
+const visor = document.querySelector(".visor")
 var elem_1 = []
 var elem_2 = []
 var elemento = 1
-var resultado;
-var termo = elem_2
+var resultado
+var termo = elem_1
 
 /* elem_1.push(9)
 elem_2.push(2) */
@@ -14,7 +14,6 @@ const termo2 = String(elem_2).split(",").join(" ").replace(/ /g ,"")
 
 
 function adicionar(value){
-    const visor = document.querySelector(".visor")
     switch (value) {
         case 0:
             termo.push(0)
@@ -47,42 +46,42 @@ function adicionar(value){
             termo.push(9)
             break;      
     }
-    const elemento1 = String(termo).split(",").join(" ").replace(/ /g ,"")
-    visor.innerHTML = `${elemento1}`
+    const elemento = String(termo).split(",").join(" ").replace(/ /g ,"")
+    visor.innerHTML = `${elemento}`
+    console.log(elem_1)
+    console.log(elem_2)
 }
 
-function sum(a = Number(termo1), b = Number(termo2)){
-    resultado = a + b;
-}
-
-function subs(a = Number(termo1), b = Number(termo2)){
-    resultado = a - b;
-}
-
-function multiply(a = Number(termo1), b = Number(termo2)){
-    resultado = a * b;
-}
-
-function divider(a = Number(termo1), b = Number(termo2)){
-    resultado = a / b;
-}
-
-function sqrt(a = Number(termo1)){
-    resultado = Math.sqrt(a);
-}
-
-function pow(a = Number(termo1) , b = 2){
-    resultado = Math.pow(a,b)
-    console.log(resultado)
+function operation(op){ 
+    switch (op) {
+        case 0:            //sum
+            termo = elem_2
+            break;
+        case 1:            //subs
+            termo = elem_2
+            break;
+        case 2:            //mult
+             termo = elem_2
+            break;
+        case 3:            //divider
+             termo = elem_2
+            break;
+        case 4:            //pow
+            termo = elem_2
+            break;
+        case 5:            //sqrt
+            termo = elem_2
+            break;
+    }
 }
 
 function clean(){
-    const visor = document.querySelector(".visor")
     visor.innerHTML = ""
     elem_1.length = 0
     elem_2.length = 0
+    termo = elem_1
 }
-
+    
 function back(){
     const visor = document.querySelector(".visor")
     termo.pop()
@@ -93,4 +92,29 @@ function back(){
 
 function result(){
     const visor = document.querySelector(".visor")
+    function sum(a = Number(termo1), b = Number(termo2)){
+        resultado = a + b;
+    }
+    
+    function subs(a = Number(termo1), b = Number(termo2)){
+        resultado = a - b;
+    }
+    
+    function multiply(a = Number(termo1), b = Number(termo2)){
+        resultado = a * b;
+    }
+    
+    function divider(a = Number(termo1), b = Number(termo2)){
+        resultado = a / b;
+    }
+    
+    function sqrt(a = Number(termo1)){
+        resultado = Math.sqrt(a);
+    }
+    
+    function pow(a = Number(termo1) , b = 2){
+        resultado = Math.pow(a,b)
+    }
+    visor.innerHTML = ""
+    visor.innerHTML = ``
 }
