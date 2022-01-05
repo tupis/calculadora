8383ed3ffa96c6d6711102c9a150dae367407861
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-01-05 17:26:37 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,15 @@
 // fazer interação do teclado com calculadora
-const visor = document.querySelector(".visor")
 var elem_1 = []
 var elem_2 = []
-var resultado
 var termo = elem_1
-
-/* elem_1.push(9)
-elem_2.push(2) */
-
+var resultado
+var operador
 const termo1 = String(elem_1).split(",").join(" ").replace(/ /g ,"")
 const termo2 = String(elem_2).split(",").join(" ").replace(/ /g ,"")
+const visor = document.querySelector(".visor")
 
 
-function adicionar(value){
+function adicionar(value) {
     switch (value) {
         case 0:
             termo.push(0)
@@ -49,67 +46,79 @@ function adicionar(value){
     visor.innerHTML = `${elemento}`
 }
 
-function operation(op){ 
+function operation(op) { 
     switch (op) {
         case 0:            //sum
             termo = elem_2
+            operador = "sum"
             break;
         case 1:            //subs
             termo = elem_2
+            operador = "subs"
             break;
         case 2:            //mult
-             termo = elem_2
+            termo = elem_2
+            operador = "mult"
             break;
         case 3:            //divider
-             termo = elem_2
+            termo = elem_2
+            operador = "divider"
             break;
         case 4:            //pow
             termo = elem_2
+            operador = "pow"
             break;
         case 5:            //sqrt
             termo = elem_2
-            break;
+            operador = "sqrt"
+        break;
     }
 }
 
-function clean(){
+function result() {
+    switch (operador) {
+        case "sum":
+            (a = Number(termo1), b = Number(termo2)) => {resultado = a + b}
+            break;
+        case "subs":
+            (a = Number(termo1), b = Number(termo2)) => {
+                resultado = a - b;
+            }
+            break;
+        
+    }
+    
+    /* function multiply(a = Number(termo1), b = Number(termo2)) {
+        resultado = a * b;
+    }
+    
+    function divider(a = Number(termo1), b = Number(termo2)) {
+        resultado = a / b;
+    }
+    
+    function sqrt(a = Number(termo1)) {
+        resultado = Math.sqrt(a);
+    }
+    
+    function pow(a = Number(termo1) , b = 2) {
+        resultado = Math.pow(a,b)
+    } */
+    visor.innerHTML = ""
+    visor.innerHTML = `${resultado}`
+}
+
+function clean() {
     visor.innerHTML = ""
     elem_1.length = 0
     elem_2.length = 0
     termo = elem_1
 }
     
-function back(){
+function back() {
     termo.pop()
     const elemento = String(termo).split(",").join(" ").replace(/ /g ,"")
     visor.innerHTML = `${elemento}`
     
 }
 
-function result(){
-    function sum(a = Number(termo1), b = Number(termo2)){
-        resultado = a + b;
-    }
-    
-    function subs(a = Number(termo1), b = Number(termo2)){
-        resultado = a - b;
-    }
-    
-    function multiply(a = Number(termo1), b = Number(termo2)){
-        resultado = a * b;
-    }
-    
-    function divider(a = Number(termo1), b = Number(termo2)){
-        resultado = a / b;
-    }
-    
-    function sqrt(a = Number(termo1)){
-        resultado = Math.sqrt(a);
-    }
-    
-    function pow(a = Number(termo1) , b = 2){
-        resultado = Math.pow(a,b)
-    }
-    visor.innerHTML = ""
-    visor.innerHTML = ``
-}
+console.log("ssalve")
