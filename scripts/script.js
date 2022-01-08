182094b28608b88f6b6f9988e3a26cce7de6aaf1
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-01-07 22:15:32 -0300

    is working

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,11 @@ var elem_2 = []
 var termo = elem_1
 var resultado
 var operador
-const termo1 = String(elem_1).split(",").join(" ").replace(/ /g ,"")
-const termo2 = String(elem_2).split(",").join(" ").replace(/ /g ,"")
+var a
+var b
+
 const visor = document.querySelector(".visor-text")
 const antecessor = document.querySelector(".antecessor")
-
 
 function adicionar(value) {
     antecessor.style.transform = "translateY(-130px)"
@@ -43,10 +43,21 @@ function adicionar(value) {
             break;
         case 9:
             termo.push(9)
-            break;      
+            break;    
+        case 10:
+            termo.push(".")  
+        break;
     }
     const elemento = String(termo).split(",").join(" ").replace(/ /g ,"")
-    visor.innerHTML = `${elemento}`
+    visor.innerHTML = `${elemento.replace(".",",")}`
+    const t1 = String(elem_1).split(",").join(" ").replace(/ /g,"")
+    const t2 = String(elem_2).split(",").join(" ").replace(/ /g ,"")
+    a = Number(t1)
+    b = Number(t2)
+
+    console.log(elem_1)
+    console.log(b)
+
 }
 
 function operation(op) { 
@@ -55,68 +66,83 @@ function operation(op) {
         case 0:            //sum
             termo = elem_2
             operador = "sum"
-            antecessor.innerHTML = `${elemento}`
+            antecessor.innerHTML = `${elemento.replace(".",",")}`
             break;
         case 1:            //subs
             termo = elem_2
             operador = "subs"
-            antecessor.innerHTML = `${elemento}`
+            antecessor.innerHTML = `${elemento.replace(".",",")}`
             break;
         case 2:            //mult
             termo = elem_2
             operador = "mult"
-            antecessor.innerHTML = `${elemento}`
+            antecessor.innerHTML = `${elemento.replace(".",",")}`
             break;
         case 3:            //divider
             termo = elem_2
             operador = "divider"
-            antecessor.innerHTML = `${elemento}`
+            antecessor.innerHTML = `${elemento.replace(".",",")}`
             break;
         case 4:            //pow
             termo = elem_2
             operador = "pow"
-            antecessor.innerHTML = `${elemento}`
+            antecessor.innerHTML = `${elemento.replace(".",",")}`
             break;
         case 5:            //sqrt
             termo = elem_2
             operador = "sqrt"
-            antecessor.innerHTML = `${elemento}`
+            antecessor.innerHTML = `${elemento.replace(".",",")}`
         break;
     }
-    console.log(operador)
 }
 
 function result() {
-    switch (operador) {
+
+    switch (operador){
         case "sum":
-            (a = Number(termo1), b = Number(termo2)) => resultado = a + b
-            
+            sum()
             break;
-        case "subs":
-            (a = Number(termo1), b = Number(termo2)) => {
-                resultado = a - b;
-            }
+       case "subs":
+            subs()
+            break;
+        case "mult":
+            multiply()
+            break;
+        case "divider":
+            divider()
+            break;
+        case "pow":
+            pow()
+            break;
+        case "sqrt":
+            sqrt()
         break;
     }
-    
+    function sum() {
+        resultado = a + b
+    }
 
-    /* function multiply(a = Number(termo1), b = Number(termo2)) {
+    function subs() {
+        resultado = a - b
+    }
+
+    function multiply() {
         resultado = a * b;
     }
     
-    function divider(a = Number(termo1), b = Number(termo2)) {
+    function divider() {
         resultado = a / b;
     }
     
-    function sqrt(a = Number(termo1)) {
+    function sqrt() {
         resultado = Math.sqrt(a);
     }
     
-    function pow(a = Number(termo1) , b = 2) {
-        resultado = Math.pow(a,b)
-    } */
+    function pow() {
+        resultado = Math.pow(a,2)
+    }
     visor.innerHTML = ""
-    visor.innerHTML = `${resultado}`
+    visor.innerHTML = `${resultado.toString().replace(".",",")}`
 }
 
 function clean() {
@@ -127,12 +153,11 @@ function clean() {
     termo = elem_1
 }
     
-function back() {
+function backspace() {
     termo.pop()
     const elemento = String(termo).split(",").join(" ").replace(/ /g ,"")
-    visor.innerHTML = `${elemento}`
+    visor.innerHTML = `${elemento.replace(".",",")}`
     if (termo.length == 0) {
         antecessor.style.transform = "translateY(-92px)"
     }
-    
 }
